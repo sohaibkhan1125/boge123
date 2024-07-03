ba443f11c277c66a0ec9735f42a06163e9869c45
sohaibkhan1125 <145782199+sohaibkhan1125@users.noreply.github.com>
2024-07-03 17:05:15 +0500

    updated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,60 +4,75 @@ import Footer from '../Footer';
 
 const Page = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <TopMenu />
-      <section className="bg-white dark:bg-gray-900">
-        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-            Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
-          </p>
-          <form action="#" className="space-y-8">
+      <h1 className='text-center text-3xl font-bold mt-5'>Contact Us</h1>
+      <main className="flex-grow flex items-center justify-center py-10">
+       
+        <form
+          id="fs-frm"
+          name="simple-contact-form"
+          acceptCharset="utf-8"
+          action="https://formspree.io/f/mnnaqvoz"
+          method="post"
+          className="w-full max-w-lg bg-white p-8 rounded-lg shadow-md"
+        >
+          <fieldset id="fs-frm-inputs" className="space-y-6">
             <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                Your email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                placeholder="name@flowbite.com"
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                Subject
-              </label>
+              <label htmlFor="full-name" className="block text-sm font-medium text-gray-700">Full Name</label>
               <input
                 type="text"
-                id="subject"
-                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                placeholder="Let us know how we can help you"
+                name="name"
+                id="full-name"
+                placeholder="First and Last"
                 required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
             </div>
-            <div className="sm:col-span-2">
-              <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
-                Your message
-              </label>
+            <div>
+              <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">Email Address</label>
+              <input
+                type="email"
+                name="_replyto"
+                id="email-address"
+                placeholder="email@domain.tld"
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
               <textarea
+                name="message"
                 id="message"
-                
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Leave a comment..."
+                placeholder="Your message here"
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               ></textarea>
             </div>
+            <input
+              type="hidden"
+              name="_subject"
+              id="email-subject"
+              value="Contact Form Submission"
+            />
+            <input
+              type="hidden"
+              name="_to"
+              value="harrisonoriahi@gmail.com"
+            />
+          </fieldset>
+          <div className="mt-6">
             <button
               type="submit"
-              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              className="w-full px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Send message
+              Submit
             </button>
-          </form>
-        </div>
-      </section>
-      <Footer/>
+          </div>
+        </form>
+      </main>
+      <Footer />
     </div>
   );
 };
